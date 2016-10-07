84d494979ac7e24cf7610bca969024422913e1f5
oschebell <oschebell@gmail.com>
2016-10-08 00:20:33 +1100

    Connect mongodb and create collection

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var bodyParser= require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var db;
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function(){
-  console.log("Listening on :3000");
+MongoClient.connect('mongodb://owens:musashi@ds053216.mlab.com:53216/trader-app', function(err, database){
+  if(err) return console.log(err)
+  db = database
+  app.listen(3000, function(){
+    console.log("Listening on :3000");
+  })
 })
 
 app.get("/", function(req, res){
@@ -13,6 +19,10 @@ app.get("/", function(req, res){
 })
 
 app.post('/notes', function(req, res){
-  console.log(req.body);
-  console.log("Is this thing on?");
+  db.collection('notes').save(req.body, function(err, result){
+    if (err) return console.log(err)
+
+    console.log("Saved to database")
+    res.redirect("/")
+  })
 })
