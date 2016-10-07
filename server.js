9a76a51a2dbcdca3471fefe838337848996bac12
oschebell <oschebell@gmail.com>
2016-10-08 00:36:01 +1100

    Implement ejs templating and render notes from db to html

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var db;
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
 MongoClient.connect('mongodb://owens:musashi@ds053216.mlab.com:53216/trader-app', function(err, database){
@@ -15,8 +16,14 @@ MongoClient.connect('mongodb://owens:musashi@ds053216.mlab.com:53216/trader-app'
 })
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  db.collection('notes').find().toArray(function(err, result){
+    if (err) return console.log(err)
+    //renders ejs
+    res.render("index.ejs", {notes:result})
+  })
 })
+
+
 
 app.post('/notes', function(req, res){
   db.collection('notes').save(req.body, function(err, result){
