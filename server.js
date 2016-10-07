d77195bc475cbbe0e3c7af2fdaf19da04c620e3b
oschebell <oschebell@gmail.com>
2016-10-08 00:02:01 +1100

    Implement post method

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var bodyParser= require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function(){
   console.log("Listening on :3000");
@@ -7,4 +10,9 @@ app.listen(3000, function(){
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/notes', function(req, res){
+  console.log(req.body);
+  console.log("Is this thing on?");
 })
